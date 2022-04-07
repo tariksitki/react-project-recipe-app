1e384b364e861b3775caf03ab1c8b049e8fc4781
tarik <63872091+tariksitki@users.noreply.github.com>
2022-04-07 02:10:14 +0200

    table completed

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -4,7 +4,7 @@ import "./Details.scss";
 const Details = () => {
   const location = useLocation();
   const meal = location.state.meal;
-  console.log(meal.recipe.totalNutrients);
+  console.log(meal.recipe);
   return (
     <main className="details-main">
       <header className="header">
@@ -15,16 +15,45 @@ const Details = () => {
       <div className="details-main-down">
         <section className="main-down-left">
           <table>
-            <tr>
-              <th>NUTRIENS</th>
-            </tr>
-            {meal.recipe.totalNutrients.map((item) => {
-              return console.log(item);
-            })}
+            <tbody>
+              <tr>
+                <th colSpan={3} className="nutriens">
+                  NUTRIENS
+                </th>
+              </tr>
+
+              {Object.values(meal.recipe.totalNutrients).map(
+                ({ label, quantity, unit }, index) => {
+                  return (
+                    <tr className="table-left-row" key={index}>
+                      <td>{label} </td>
+                      <td className="quantity">{quantity} </td>
+                      <td className="unit">{unit} </td>
+                    </tr>
+                  );
+                }
+              )}
+            </tbody>
           </table>
         </section>
 
-        <section className="main-down-right"></section>
+        <section className="main-down-right">
+          <table>
+            <tbody>
+              <tr>
+                <th>INGREDIENTS</th>
+              </tr>
+
+              {meal.recipe.ingredients.map((ingredient, index) => {
+                return (
+                  <tr key={index}>
+                    <td className="ingredient-td">* {ingredient.text} </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </section>
       </div>
     </main>
   );
